cf8d146ebaca76dfa5112a109eb7278f32d2f117
jmatdawg <matthews_jakiera@yahoo.com>
2018-03-12 10:17:52 -0500

    film and people

--- src/components/Filmcard.jsx
+++ src/components/Filmcard.jsx
@@ -5,8 +5,8 @@ class Filmcard extends Component {
         return (
             <div className="card" key={this.props.film.id}>
                 <div className="card-block">
-                    <h4 className="card-title">Title: { this.props.film.title }</h4>
-                    <h6 className="card-subtitle mb-2 text-muted">Release Date: { this.props.film.release_date }</h6>
+                    <h4 className="card-title">Film Title: { this.props.film.title }</h4>
+                    <h6 className="card-subtitle mb-2 text-muted">Film Director: { this.props.film.director }</h6>
                     <p className="card-text">Desrciption: { this.props.film.description }</p>
                 </div>
             </div>
